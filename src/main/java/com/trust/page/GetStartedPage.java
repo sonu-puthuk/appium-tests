f0c04bcb01b491c211fa15b84264e09601229d1f
sonu-puthuk <sonu.sdz@gmail.com>
2024-01-21 03:06:31 +0400

    handle error screen

--- src/main/java/com/trust/page/GetStartedPage.java
+++ src/main/java/com/trust/page/GetStartedPage.java
@@ -6,17 +6,20 @@ import org.openqa.selenium.WebElement;
 
 public class GetStartedPage extends BasePage {
     private final By getStarted = By.className("android.widget.Button");
+    private final By errorMsg = By.xpath("//android.widget.TextView[@text=\"DEVICE SECURITY COMPROMISED\"]");
 
     public GetStartedPage(AppiumDriver driver) {
         super(driver);
     }
 
     public HomePage tapGetStarted() {
+        if (getElement(errorMsg).isDisplayed()) {
+            new SecurityErrorPage(driver).tapOK();
+        }
         WebElement getStartedElem = getElement(getStarted);
         waitForElementToBeVisible(getStartedElem);
 
         click(getStartedElem);
-
         return new HomePage(driver);
     }
 
