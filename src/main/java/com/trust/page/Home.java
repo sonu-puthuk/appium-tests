64440dc4bd83a91586fe993d06d3833af504b68b
sonu-puthuk <sonu.sdz@gmail.com>
2024-01-20 18:02:34 +0400

    fix tests

--- src/main/java/com/trust/page/Home.java
+++ src/main/java/com/trust/page/Home.java
@@ -8,12 +8,13 @@ public class Home extends BasePage{
         super(driver);
     }
 
-    private final By createNewWallet = By.linkText("Create new wallet");
-    private final By addExistingWallet = By.linkText("Add existing wallet");
+    private final By popularTokens = By.xpath("//android.widget.TextView[@text=\"Popular tokens\"]");
+    private final By createNewWallet = By.xpath("//android.widget.TextView[@text=\"Create new wallet\"]");
+    private final By addExistingWallet = By.xpath("//android.widget.TextView[@text=\"Add existing wallet\"]");
 
     public boolean isLoaded(){
-        waitForElementToBeVisible(getElement(createNewWallet));
-        return getElement(createNewWallet).isDisplayed();
+        waitForElementToBeVisible(getElement(popularTokens));
+        return getElement(popularTokens).isDisplayed();
     }
 
 
